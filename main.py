df6cba808c3834a08b5ffe4b7ca5356270535cfa
Eric <eric-holdener>
2022-02-16 14:03:19 -0800

    Twitter scraper made using Twint, variables to control the keyword, number of tweets, and final file name used. Outputs to a csv then turned into a dataframe in pandas. Next to figure out how to do sentiment analysis

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 # This is a sample Python script.
+import twitterScraper
+import pandas as pd
+
+def main():
+    keyword = 'Kanye'
+    tweetsToScrape = 1000
+    fileName = 'sent_analysis_tweets.csv'
+    twitterScraper.scrape(keyword, tweetsToScrape, fileName)
+    df = pd.read_csv(fileName)
+    print(df)
+
+if __name__ == '__main__':
+    main()
 
 
---
+++ twitterScraper.py
@@ -0,0 +1,17 @@
+import twint
+
+
+def scrape(keyword, scope, fileName):
+    c = twint.Config()
+
+    # topic to scrape
+    c.Search =[keyword]
+
+    # number of tweets to scrape
+    c.Limit = scope
+
+    c.Store_csv = True
+    c.Output = fileName
+
+    twint.run.Search(c)
+
