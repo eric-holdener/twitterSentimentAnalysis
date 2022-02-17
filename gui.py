2572cbdec55859808ef0cf76e31cbacd92ce830d
Eric <eric-holdener>
2022-02-16 19:19:10 -0800

    adding file for gui, will work on it once sent analysis is done

---
+++ gui.py
@@ -0,0 +1,2 @@
+# use this file to create the gui to select # of tweets to scrape, keyword, and filename - beginning
+# spit out a wordcloud net, as well as visualizations on the sentiment in the gui
